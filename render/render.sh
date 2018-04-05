250f60a48a58b96cd46ebdfb7b5a4c769bd5e8d7
ern0 <ern0@linkbroker.hu>
2018-04-06 00:09:31 +0200

    layoud RC

--- render/render.sh
+++ render/render.sh
@@ -1,55 +1,28 @@
 #!/bin/bash
 clear
 
+if [[ -f /usr/bin/xvfb-run ]]; then
+	xvfb-run -a --server-args="-screen 0, 800x600x24" \
+		wkhtmltoimage -q meetingroom.html _raw.png
+else
+	wkhtmltoimage -q meetingroom.html _raw.png
+fi
 
-function main {
+rm -f meetingroom.png
 
-	if [[ -f /usr/bin/xvfb-run ]]; then
-		xvfb-run -a --server-args="-screen 0, 800x600x24" \
-			wkhtmltoimage -q meetingroom.html _raw.png
-	else
-		wkhtmltoimage --zoom 1.0 -q meetingroom.html _raw.png
-	fi
+convert _raw.png \
+	-crop 200x200+0+0 \
+	+dither \
+	-colors 2 \
+	-quality 100 \
+	-type bilevel \
+	meetingroom.png
 
-	rm -f meetingroom.png
+convert meetingroom.png \
+	-define histogram:unique-colors=true \
+	-format %c histogram:info:- \
+	| wc -l
 
-	#############
-	c1
-	#############
+file meetingroom.png
 
-	convert meetingroom.png \
-		-define histogram:unique-colors=true \
-		-format %c histogram:info:- \
-		| wc -l
-
-	file meetingroom.png
-
-	rm _raw.png
-}
-
-
-function c1 {
-
-	convert _raw.png \
-		-resize 50% \
-		-crop 200x200+0+0 \
-		+dither \
-		-colors 2 \
-		-quality 100 \
-		-type bilevel \
-		meetingroom.png
-}
-
-
-function c2 {
-
-	convert _raw.png \
-		-resize 50% \
-		-crop 200x200+0+0 \
-		-type grayscale \
-		-threshold 70% \
-		meetingroom.png
-}
-
-
-main
+rm _raw.png
