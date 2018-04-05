edd69a0e73cbc3484dace5a05b2e5d06e9310411
ern0 <ern0@linkbroker.hu>
2018-04-05 20:37:39 +0200

    better render script

--- render/render.sh
+++ render/render.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 clear
 
-if [[ -f /usr/bin/xvfb-run ]]; then
-	xvfb-run -a --server-args="-screen 0, 800x600x24" \
+
+function main {
+
+	if [[ -f /usr/bin/xvfb-run ]]; then
+		xvfb-run -a --server-args="-screen 0, 800x600x24" \
+			wkhtmltoimage -q meetingroom.html _raw.png
+	else
 		wkhtmltoimage -q meetingroom.html _raw.png
-else
-	wkhtmltoimage -q meetingroom.html _raw.png
-fi
+	fi
 
-convert _raw.png \
-	-resize 50% \
-	-crop 200x200+0+0 \
-	+dither \
-	-colors 2 \
-	-quality 100 \
-	-type bilevel \
-	meetingroom.png
+	#############
+	c1
+	#############
 
-convert meetingroom.png \
-	-define histogram:unique-colors=true \
-	-format %c histogram:info:-
+	convert meetingroom.png \
+		-define histogram:unique-colors=true \
+		-format %c histogram:info:-
 
-file meetingroom.png
+	file meetingroom.png
 
-rm _raw.png
+	rm _raw.png
+}
+
+
+function c1 {
+
+	convert _raw.png \
+		-resize 50% \
+		-crop 200x200+0+0 \
+		+dither \
+		-colors 2 \
+		-quality 100 \
+		-type bilevel \
+		meetingroom.png
+}
+
+
+function c2 {
+
+	convert _raw.png \
+		-resize 50% \
+		-crop 200x200+0+0 \
+		-colors 2 \
+		-quality 100 \
+		-type bilevel \
+		meetingroom.png
+}
+
+
+main
