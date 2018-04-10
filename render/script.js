3ee9f5ea00036eea50826904ac3cea84587cdfbe
ern0 <ern0@linkbroker.hu>
2018-04-10 23:49:30 +0200

    Design RC with responsive title

--- render/script.js
+++ render/script.js
@@ -1,2 +1,12 @@
-// window.onload = refresh;
+window.onload = function() {
+
+	elm = document.getElementById("title");
+	title = elm.innerHTML;
+	len = title.trim().length;
+	
+	if (len > 9) elm.style.fontSize = 17;
+	if (len > 10) elm.style.fontSize = 16;
+	if (len > 12) elm.style.fontSize = 15;
+
+}
 
