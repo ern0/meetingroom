e7339da80e97b0433db902be8968fb6b10c86da9
ern0 <ern0@linkbroker.hu>
2018-04-12 16:15:59 +0200

    minor: polished

--- render/render.py
+++ render/render.py
@@ -11,14 +11,26 @@ class Render:
 	def main(self):
 
 		self.about()
+		self.process(sys.argv[1],sys.argv[2])
 
 
 	def about(self):
+
 		if len(sys.argv) == 2: return
+
 		print(
 			os.path.basename(sys.argv[0])
 			+ " <data.json> <output.png>"
-		) 
+		)
+		os._exit(1)
+
+
+	def process(self,jfn,rfn):
+
+		self.jsonFileName = jfn
+		self.resultFileName = rfn
+
+		print("blah blah, processing... " + jfn + " => " + rnf)
 
 
 if __name__ == "__main__":
